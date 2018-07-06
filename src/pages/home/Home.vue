6d1868ad6efe58fd6ffb518881b0fea55fd98756
hellobq <2279585790@qq.com>
2018-07-06 15:09:58 +0800

    travel init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'home page!'
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+
+</style>
---
+++ src/pages/home/components/Search.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home-Search'
+}
+</script>
+
+<style scoped lang="stylus">
+
+</style>
