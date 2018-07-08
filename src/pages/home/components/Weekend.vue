e4bd7c1b76a0322c1e1f916f148192650fe62699
hellobq <2279585790@qq.com>
2018-07-08 22:15:09 +0800

    home weekend

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home-weekend">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home-weekend'
+}
+</script>
+
+<style scoped lang="stylus">
+
+</style>
