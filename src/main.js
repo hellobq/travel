4ee790f32b1b20c2d2d0334f8e90c8ff669155fd
hellobq <2279585790@qq.com>
2019-07-03 14:48:42 +0800

    test jenkins

--- src/main.js
+++ src/main.js
@@ -1,28 +1,12 @@
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-import '@/assets/styles/reset.css'
-import '@/assets/styles/border.css'
-import '@/assets/styles/iconfont.css'
-import 'swiper/dist/css/swiper.css'
+Vue.config.productionTip = false;
 
-import fastClick from 'fastclick'
-import VueLazyload from 'vue-lazyload'
-
-Vue.use(VueLazyload, {
-  preLoad: 1.0,
-  listenEvents: ['scroll']
-})
-Vue.config.productionTip = false
-fastClick.attach(document.body)
-
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
   store,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App),
+}).$mount('#app');
