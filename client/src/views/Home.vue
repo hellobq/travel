659a31d89889a92106cf20b620c8f695a5eb8bb9
baiqiang <2279585890@qq.com>
2020-11-11 11:46:59 +0800

    upload 3 jpgs

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/art-babochki-nasekomye-risunki-9226.jpg">
+    <img alt="Vue logo" src="../assets/babochki-cvety-nasekomye-pejzazh-priroda-41836.jpg">
+    <img alt="Vue logo" src="../assets/babochki-cvety-priroda-rasteniya-7.jpg">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
